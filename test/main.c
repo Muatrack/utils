30012d9ed13269f325e208d62671e47d0cda1e2d
moli <fjfhccfkuk@163.com>
2024-12-01 15:47:18 +0800

    增加 测试函数调用

--- test/main.c
+++ test/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils.h"
+#include <unistd.h>
+#include <time.h>
+
+#define T_INTERVAL_TMS  5000
 
 void main() {
+    uint32_t    _lastTms = 0;
     printf("[%s]\n", __func__);
+    
+    while( true ) {        
+        if( utils_is_time_ms_escaped_in_day( &_lastTms, T_INTERVAL_TMS) == true ) {
+            printf("[ UTILS_TMS-RING ] clock ts: %ld \n", time(NULL));
+        }
+
+        usleep(200000);
+    }
 }
