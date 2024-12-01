75bd4b4f5b0fd4ef6d3249871a1fe175448e9aef
moli <fjfhccfkuk@163.com>
2024-12-01 21:24:23 +0800

    update

--- test/test_timer.c
+++ test/test_timer.c
@@ -12,7 +12,7 @@ int main(char argv[], int argc) {
 
     printf("[%s] args count:%d\n", __func__, argc);
     
-    while( true ) {        
+    while( true ) {
         if( utils_is_time_ms_escaped_in_day( &_lastTms, T_INTERVAL_TMS) == true ) {
             printf("[ UTILS_TMS-RING ] clock ts: %ld \n", time(NULL));
         }
